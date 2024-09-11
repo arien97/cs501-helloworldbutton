db7d29cab451062fc34189e15e6cdb4518ee917b
arien <aramin@bu.edu>
2024-09-10 20:30:09 -0400

    hello world app with button

---
+++ HelloWorldButton/app/src/main/java/com/example/helloworldbutton/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.helloworldbutton
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.helloworldbutton.ui.theme.HelloWorldButtonTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+//            HelloWorldButtonTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "World",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+//            }
+            HWButton()
+        }
+    }
+}
+@Composable
+fun HWButton(){
+    var num = remember { mutableIntStateOf(0) }
+
+    Button( modifier = Modifier.padding(all = 100.dp) ,
+        onClick = {
+            num.intValue += 1
+
+    }) {
+        Text(text = "Click Me")
+
+    }
+
+    if (num.intValue % 2 == 1) {
+        Column(modifier = Modifier.padding(horizontal = 120.dp, vertical = 160.dp)) {
+            Text(
+                text = "Hello World!"
+
+            )
+        }
+
+    }
+
+
+}
+
+
+//@Composable
+//fun Greeting(name: String) {
+//    Text(
+//        text = "Hello $name!"
+//    )
+//}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HWButton()
+}
